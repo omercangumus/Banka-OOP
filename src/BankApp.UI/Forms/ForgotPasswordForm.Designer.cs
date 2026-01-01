e80f148ca6d20e4270a2df9cad4fb847cf7d5fe5
Ömer Can Gümüş <omercangumus3@gmail.com>
2026-01-01 16:41:31 +0300

    refactor: ForgotPasswordForm - Firat Universitesi standartlari
    DEGISIKLIKLER:
    
    1. ForgotPasswordForm.Designer.cs:
       - Segoe UI -> Tahoma font
       - txtEmail -> txtEposta
       - txtCode -> txtKod
       - txtNewPassword -> txtYeniSifre
       - btnSendCode -> btnKodGonder
       - btnVerify -> btnDogrula
       - btnChangePassword -> btnSifreDegistir
       - lblTitle -> lblBaslik
       - lblInstruction -> lblAciklama
    
    2. ForgotPasswordForm.cs:
       - Yeni kontrol isimleri uyumu
       - XML summaries eklendi

--- src/BankApp.UI/Forms/ForgotPasswordForm.Designer.cs
+++ src/BankApp.UI/Forms/ForgotPasswordForm.Designer.cs
@@ -5,18 +5,22 @@ using DevExpress.XtraEditors;
 
 namespace BankApp.UI.Forms
 {
+    /// <summary>
+    /// Şifre sıfırlama formu tasarımcı kodu
+    /// Created by Fırat Üniversitesi Standartları, 01/01/2026
+    /// </summary>
     public partial class ForgotPasswordForm
     {
         private IContainer components = null;
         private Panel pnlGradient;
-        private LabelControl lblTitle;
-        private LabelControl lblInstruction;
-        private TextEdit txtEmail;
-        private SimpleButton btnSendCode;
-        private TextEdit txtCode;
-        private SimpleButton btnVerify;
-        private TextEdit txtNewPassword;
-        private SimpleButton btnChangePassword;
+        private LabelControl lblBaslik;
+        private LabelControl lblAciklama;
+        private TextEdit txtEposta;
+        private SimpleButton btnKodGonder;
+        private TextEdit txtKod;
+        private SimpleButton btnDogrula;
+        private TextEdit txtYeniSifre;
+        private SimpleButton btnSifreDegistir;
 
         protected override void Dispose(bool disposing)
         {
@@ -27,111 +31,111 @@ namespace BankApp.UI.Forms
         private void InitializeComponent()
         {
             this.pnlGradient = new Panel();
-            this.lblTitle = new LabelControl();
-            this.lblInstruction = new LabelControl();
-            this.txtEmail = new TextEdit();
-            this.btnSendCode = new SimpleButton();
-            this.txtCode = new TextEdit();
-            this.btnVerify = new SimpleButton();
-            this.txtNewPassword = new TextEdit();
-            this.btnChangePassword = new SimpleButton();
+            this.lblBaslik = new LabelControl();
+            this.lblAciklama = new LabelControl();
+            this.txtEposta = new TextEdit();
+            this.btnKodGonder = new SimpleButton();
+            this.txtKod = new TextEdit();
+            this.btnDogrula = new SimpleButton();
+            this.txtYeniSifre = new TextEdit();
+            this.btnSifreDegistir = new SimpleButton();
 
-            ((ISupportInitialize)(this.txtEmail.Properties)).BeginInit();
-            ((ISupportInitialize)(this.txtCode.Properties)).BeginInit();
-            ((ISupportInitialize)(this.txtNewPassword.Properties)).BeginInit();
+            ((ISupportInitialize)(this.txtEposta.Properties)).BeginInit();
+            ((ISupportInitialize)(this.txtKod.Properties)).BeginInit();
+            ((ISupportInitialize)(this.txtYeniSifre.Properties)).BeginInit();
             this.SuspendLayout();
 
             // pnlGradient
             this.pnlGradient.BackColor = Color.FromArgb(26, 54, 93);
             this.pnlGradient.Dock = DockStyle.Fill;
-            this.pnlGradient.Controls.Add(this.btnChangePassword);
-            this.pnlGradient.Controls.Add(this.txtNewPassword);
-            this.pnlGradient.Controls.Add(this.btnVerify);
-            this.pnlGradient.Controls.Add(this.txtCode);
-            this.pnlGradient.Controls.Add(this.btnSendCode);
-            this.pnlGradient.Controls.Add(this.txtEmail);
-            this.pnlGradient.Controls.Add(this.lblInstruction);
-            this.pnlGradient.Controls.Add(this.lblTitle);
+            this.pnlGradient.Controls.Add(this.btnSifreDegistir);
+            this.pnlGradient.Controls.Add(this.txtYeniSifre);
+            this.pnlGradient.Controls.Add(this.btnDogrula);
+            this.pnlGradient.Controls.Add(this.txtKod);
+            this.pnlGradient.Controls.Add(this.btnKodGonder);
+            this.pnlGradient.Controls.Add(this.txtEposta);
+            this.pnlGradient.Controls.Add(this.lblAciklama);
+            this.pnlGradient.Controls.Add(this.lblBaslik);
 
-            // lblTitle
-            this.lblTitle.Location = new Point(130, 20);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Text = "ŞİFRE SIFIRLAMA";
-            this.lblTitle.Appearance.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
-            this.lblTitle.Appearance.ForeColor = Color.FromArgb(212, 175, 55);
+            // lblBaslik (Fırat Standardı: Tahoma font)
+            this.lblBaslik.Location = new Point(130, 20);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Text = "ŞİFRE SIFIRLAMA";
+            this.lblBaslik.Appearance.Font = new Font("Tahoma", 16F, FontStyle.Bold);
+            this.lblBaslik.Appearance.ForeColor = Color.FromArgb(212, 175, 55);
 
-            // lblInstruction
-            this.lblInstruction.Location = new Point(80, 65);
-            this.lblInstruction.Name = "lblInstruction";
-            this.lblInstruction.Text = "Lütfen kayıtlı e-posta adresinizi giriniz.";
-            this.lblInstruction.Appearance.Font = new Font("Segoe UI", 10F);
-            this.lblInstruction.Appearance.ForeColor = Color.White;
+            // lblAciklama (Fırat Standardı: Tahoma font)
+            this.lblAciklama.Location = new Point(80, 65);
+            this.lblAciklama.Name = "lblAciklama";
+            this.lblAciklama.Text = "Lütfen kayıtlı e-posta adresinizi giriniz.";
+            this.lblAciklama.Appearance.Font = new Font("Tahoma", 9F);
+            this.lblAciklama.Appearance.ForeColor = Color.White;
 
-            // txtEmail
-            this.txtEmail.Location = new Point(80, 100);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Properties.NullValuePrompt = "E-Posta Adresi";
-            this.txtEmail.Properties.NullValuePromptShowForEmptyValue = true;
-            this.txtEmail.Size = new Size(250, 34);
-            this.txtEmail.Properties.Appearance.Font = new Font("Segoe UI", 11F);
+            // txtEposta (Fırat Standardı: txt prefix, Tahoma font)
+            this.txtEposta.Location = new Point(80, 100);
+            this.txtEposta.Name = "txtEposta";
+            this.txtEposta.Properties.NullValuePrompt = "E-Posta Adresi";
+            this.txtEposta.Properties.NullValuePromptShowForEmptyValue = true;
+            this.txtEposta.Size = new Size(250, 34);
+            this.txtEposta.Properties.Appearance.Font = new Font("Tahoma", 10F);
 
-            // btnSendCode
-            this.btnSendCode.Location = new Point(340, 100);
-            this.btnSendCode.Name = "btnSendCode";
-            this.btnSendCode.Size = new Size(100, 34);
-            this.btnSendCode.Text = "KOD GÖNDER";
-            this.btnSendCode.Appearance.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            this.btnSendCode.Appearance.BackColor = Color.FromArgb(44, 82, 130);
-            this.btnSendCode.Appearance.ForeColor = Color.White;
-            this.btnSendCode.Appearance.Options.UseBackColor = true;
-            this.btnSendCode.Appearance.Options.UseForeColor = true;
-            this.btnSendCode.Click += new System.EventHandler(this.btnSendCode_Click);
+            // btnKodGonder (Fırat Standardı: btn prefix, Tahoma font)
+            this.btnKodGonder.Location = new Point(340, 100);
+            this.btnKodGonder.Name = "btnKodGonder";
+            this.btnKodGonder.Size = new Size(100, 34);
+            this.btnKodGonder.Text = "KOD GÖNDER";
+            this.btnKodGonder.Appearance.Font = new Font("Tahoma", 8.25F, FontStyle.Bold);
+            this.btnKodGonder.Appearance.BackColor = Color.FromArgb(44, 82, 130);
+            this.btnKodGonder.Appearance.ForeColor = Color.White;
+            this.btnKodGonder.Appearance.Options.UseBackColor = true;
+            this.btnKodGonder.Appearance.Options.UseForeColor = true;
+            this.btnKodGonder.Click += new System.EventHandler(this.btnKodGonder_Click);
 
-            // txtCode
-            this.txtCode.Location = new Point(80, 150);
-            this.txtCode.Name = "txtCode";
-            this.txtCode.Properties.NullValuePrompt = "Doğrulama Kodu";
-            this.txtCode.Properties.NullValuePromptShowForEmptyValue = true;
-            this.txtCode.Size = new Size(150, 34);
-            this.txtCode.Properties.Appearance.Font = new Font("Segoe UI", 11F);
-            this.txtCode.Enabled = false;
+            // txtKod (Fırat Standardı: txt prefix, Tahoma font)
+            this.txtKod.Location = new Point(80, 150);
+            this.txtKod.Name = "txtKod";
+            this.txtKod.Properties.NullValuePrompt = "Doğrulama Kodu";
+            this.txtKod.Properties.NullValuePromptShowForEmptyValue = true;
+            this.txtKod.Size = new Size(150, 34);
+            this.txtKod.Properties.Appearance.Font = new Font("Tahoma", 10F);
+            this.txtKod.Enabled = false;
 
-            // btnVerify
-            this.btnVerify.Location = new Point(240, 150);
-            this.btnVerify.Name = "btnVerify";
-            this.btnVerify.Size = new Size(100, 34);
-            this.btnVerify.Text = "DOĞRULA";
-            this.btnVerify.Appearance.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            this.btnVerify.Appearance.BackColor = Color.FromArgb(44, 82, 130);
-            this.btnVerify.Appearance.ForeColor = Color.White;
-            this.btnVerify.Appearance.Options.UseBackColor = true;
-            this.btnVerify.Appearance.Options.UseForeColor = true;
-            this.btnVerify.Enabled = false;
-            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // btnDogrula (Fırat Standardı: btn prefix, Tahoma font)
+            this.btnDogrula.Location = new Point(240, 150);
+            this.btnDogrula.Name = "btnDogrula";
+            this.btnDogrula.Size = new Size(100, 34);
+            this.btnDogrula.Text = "DOĞRULA";
+            this.btnDogrula.Appearance.Font = new Font("Tahoma", 8.25F, FontStyle.Bold);
+            this.btnDogrula.Appearance.BackColor = Color.FromArgb(44, 82, 130);
+            this.btnDogrula.Appearance.ForeColor = Color.White;
+            this.btnDogrula.Appearance.Options.UseBackColor = true;
+            this.btnDogrula.Appearance.Options.UseForeColor = true;
+            this.btnDogrula.Enabled = false;
+            this.btnDogrula.Click += new System.EventHandler(this.btnDogrula_Click);
 
-            // txtNewPassword
-            this.txtNewPassword.Location = new Point(80, 200);
-            this.txtNewPassword.Name = "txtNewPassword";
-            this.txtNewPassword.Properties.NullValuePrompt = "Yeni Şifre";
-            this.txtNewPassword.Properties.NullValuePromptShowForEmptyValue = true;
-            this.txtNewPassword.Properties.UseSystemPasswordChar = true;
-            this.txtNewPassword.Size = new Size(260, 34);
-            this.txtNewPassword.Properties.Appearance.Font = new Font("Segoe UI", 11F);
-            this.txtNewPassword.Enabled = false;
+            // txtYeniSifre (Fırat Standardı: txt prefix, Tahoma font)
+            this.txtYeniSifre.Location = new Point(80, 200);
+            this.txtYeniSifre.Name = "txtYeniSifre";
+            this.txtYeniSifre.Properties.NullValuePrompt = "Yeni Şifre";
+            this.txtYeniSifre.Properties.NullValuePromptShowForEmptyValue = true;
+            this.txtYeniSifre.Properties.UseSystemPasswordChar = true;
+            this.txtYeniSifre.Size = new Size(260, 34);
+            this.txtYeniSifre.Properties.Appearance.Font = new Font("Tahoma", 10F);
+            this.txtYeniSifre.Enabled = false;
 
-            // btnChangePassword
-            this.btnChangePassword.Location = new Point(80, 250);
-            this.btnChangePassword.Name = "btnChangePassword";
-            this.btnChangePassword.Size = new Size(260, 45);
-            this.btnChangePassword.Text = "ŞİFREYİ DEĞİŞTİR";
-            this.btnChangePassword.Appearance.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            this.btnChangePassword.Appearance.BackColor = Color.FromArgb(212, 175, 55);
-            this.btnChangePassword.Appearance.ForeColor = Color.FromArgb(26, 54, 93);
-            this.btnChangePassword.Appearance.Options.UseBackColor = true;
-            this.btnChangePassword.Appearance.Options.UseForeColor = true;
-            this.btnChangePassword.Appearance.Options.UseFont = true;
-            this.btnChangePassword.Enabled = false;
-            this.btnChangePassword.Click += new System.EventHandler(this.btnChangePassword_Click);
+            // btnSifreDegistir (Fırat Standardı: btn prefix, Tahoma font)
+            this.btnSifreDegistir.Location = new Point(80, 250);
+            this.btnSifreDegistir.Name = "btnSifreDegistir";
+            this.btnSifreDegistir.Size = new Size(260, 45);
+            this.btnSifreDegistir.Text = "ŞİFREYİ DEĞİŞTİR";
+            this.btnSifreDegistir.Appearance.Font = new Font("Tahoma", 11F, FontStyle.Bold);
+            this.btnSifreDegistir.Appearance.BackColor = Color.FromArgb(212, 175, 55);
+            this.btnSifreDegistir.Appearance.ForeColor = Color.FromArgb(26, 54, 93);
+            this.btnSifreDegistir.Appearance.Options.UseBackColor = true;
+            this.btnSifreDegistir.Appearance.Options.UseForeColor = true;
+            this.btnSifreDegistir.Appearance.Options.UseFont = true;
+            this.btnSifreDegistir.Enabled = false;
+            this.btnSifreDegistir.Click += new System.EventHandler(this.btnSifreDegistir_Click);
 
             // ForgotPasswordForm
             this.AutoScaleDimensions = new SizeF(7F, 15F);
@@ -144,11 +148,10 @@ namespace BankApp.UI.Forms
             this.Text = "NovaBank - Şifre Sıfırlama";
             this.StartPosition = FormStartPosition.CenterParent;
 
-            ((ISupportInitialize)(this.txtEmail.Properties)).EndInit();
-            ((ISupportInitialize)(this.txtCode.Properties)).EndInit();
-            ((ISupportInitialize)(this.txtNewPassword.Properties)).EndInit();
+            ((ISupportInitialize)(this.txtEposta.Properties)).EndInit();
+            ((ISupportInitialize)(this.txtKod.Properties)).EndInit();
+            ((ISupportInitialize)(this.txtYeniSifre.Properties)).EndInit();
             this.ResumeLayout(false);
         }
     }
 }
-
